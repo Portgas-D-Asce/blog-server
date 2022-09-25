1875fbd8d1ca8bf80e124f69006827a78336bb6b
pk <pk@gmail.com>
2022-09-26 03:47:56 +0800

    删除无用导包

--- src/main/java/com/devil/blog/controller/CategoryController.java
+++ src/main/java/com/devil/blog/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.devil.blog.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/devil/blog/service/ArticleService.java
+++ src/main/java/com/devil/blog/service/ArticleService.java
@@ -1,8 +1,5 @@
 package com.devil.blog.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.devil.blog.entity.Article;
 
 
