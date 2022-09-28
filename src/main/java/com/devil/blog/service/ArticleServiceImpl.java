71d94dd77744b3ae78942169c0ac5abb03f9be08
pk <pk@gmail.com>
2022-09-28 13:19:50 +0800

    简单实现获取文章内容接口

--- src/main/java/com/devil/blog/service/ArticleServiceImpl.java
+++ src/main/java/com/devil/blog/service/ArticleServiceImpl.java
@@ -1,5 +1,10 @@
 package com.devil.blog.service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +24,25 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public String getContent(int id) {
-        return "<h1>Hello World!!! </h1>";
+        Article article = articleMapper.getArticle(id);
+        String name = article.getName();
+        File file = new File("/Users/pk/Blog/article/" + name + "/" + name + ".md");
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String temp = "";
+            while((temp = bufferedReader.readLine()) != null) {
+                stringBuilder.append(temp + "\n");
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(stringBuilder.toString());
+        
+        return stringBuilder.toString();
     }
     
 }
